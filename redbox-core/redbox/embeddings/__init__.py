a97c30a6a59db6a8c6ae7eb3894befef5fe071bd
Anthoni Gleeson <anthoni.gleeson@digital.trade.gov.uk>
2024-08-14 09:35:01 +0100

    using old sentence transformers implementation

--- redbox-core/redbox/embeddings/__init__.py
+++ redbox-core/redbox/embeddings/__init__.py
@@ -1,5 +1,5 @@
+from langchain_community.embeddings import SentenceTransformerEmbeddings
 from langchain_core.embeddings import Embeddings
-from langchain_huggingface import HuggingFaceEmbeddings
 from redbox.models.settings import Settings
 
 from .langchain import get_azure_embeddings, get_openai_embeddings
@@ -11,7 +11,7 @@ def get_embeddings(env: Settings) -> Embeddings:
     elif env.embedding_backend == "openai":
         return get_openai_embeddings(env)
     elif env.embedding_backend == "sentencetransformers":
-        return HuggingFaceEmbeddings(
+        return SentenceTransformerEmbeddings(
             model_name=env.embedding_openai_model,
         )
     else:
